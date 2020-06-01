f68d8a22465d59632b39495e105e73dc79423448
Greg Findon <greg.findon@playnicely.com>
2020-06-01 18:40:05 +0100

    Removed return false on case TXXX for ID3v2

--- getid3/write.php
+++ getid3/write.php
@@ -636,7 +636,7 @@ class getid3_writetags
 							$tag_data_id3v2['TXXX'][] = $txxx_data_array;
 						} else {
 							$this->errors[] = 'ID3v2 TXXX data is not properly structured';
-							return false;
+							//return false;
 						}
 					}
 					break;
